0780a69b6076b889d3c0216490f8ec5d53cf4fea
Philipp Kutin <philipp@swarm64.com>
2019-02-27 15:58:13 +0100

    util.lua: stylistic tweaks.

--- util.lua
+++ util.lua
@@ -9,22 +9,27 @@ local unpack = unpack
 
 ----------
 
+local api = {}
+
 -- Wrap 'error' in assert-like call to write type checks in one line instead of three.
-local function check(pred, msg, level)
+function api.check(pred, msg, level)
     if (not pred) then
         error(msg, level+1)
     end
 end
 
+local check = api.check
+
 -- argstab = splitAtWhitespace(args)
-local function splitAtWhitespace(args)
-    -- TODO: use 'check()' if this is an API function? Or remove it from the API?
-    assert(type(args) == "string")
+function api.splitAtWhitespace(args)
+    check(type(args) == "string", "<args> must be a string", 2)
+
     local argstab = {}
     -- Split delimited by whitespace.
     for str in args:gmatch("[^%s]+") do
         argstab[#argstab+1] = str
     end
+
     return argstab
 end
 
@@ -41,7 +46,7 @@ local function iscellstr(tab)
     return (tab[0] == nil)
 end
 
-local function check_iftab_iscellstr(tab, name, level)
+function api.check_iftab_iscellstr(tab, name, level)
     if (type(tab)=="table") then
         if (not iscellstr(tab)) then
             error(name.." must be a string sequence when a table, with no element at [0]", level+1)
@@ -49,18 +54,18 @@ local function check_iftab_iscellstr(tab, name, level)
     end
 end
 
-local function checkOptionsArgAndGetDefault(opts, defaultValue)
+function api.checkOptionsArgAndGetDefault(opts, defaultValue)
     if (opts == nil) then
         opts = defaultValue;
     else
         check(type(opts)=="number" or type(opts)=="table", 3)
-        check_iftab_iscellstr(opts, "<opts>", 3)
+        api.check_iftab_iscellstr(opts, "<opts>", 3)
     end
 
     return opts
 end
 
-local function handleTableOfOptionStrings(lib, prefix, opts)
+function api.handleTableOfOptionStrings(lib, prefix, opts)
     assert(type(prefix) == "string")
 
     if (type(opts)=="table") then
@@ -74,11 +79,5 @@ local function handleTableOfOptionStrings(lib, prefix, opts)
     return opts
 end
 
--- Return API table.
-return {
-    check = check,
-    splitAtWhitespace = splitAtWhitespace,
-    check_iftab_iscellstr = check_iftab_iscellstr,
-    checkOptionsArgAndGetDefault = checkOptionsArgAndGetDefault,
-    handleTableOfOptionStrings = handleTableOfOptionStrings,
-}
+-- Done!
+return api
