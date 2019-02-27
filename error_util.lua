b0a12e41ffdaf17d0e293a74fe183fa78d694210
Philipp Kutin <philipp@swarm64.com>
2019-02-27 17:40:55 +0100

    error_util.lua: add checktype()

--- error_util.lua
+++ error_util.lua
@@ -1,4 +1,6 @@
+local assert = assert
 local error = error
+local type = type
 
 ----------
 
@@ -7,6 +9,19 @@ local api = {}
 -- Wrap 'error' in assert-like call to write type checks in one line instead of three.
 function api.check(pred, msg, level)
     if (not pred) then
+        error(msg, level+1)
+    end
+end
+
+function api.checktype(object, argIdx, typename, level)
+    -- NOTE: type(nil) returns nil. We disallow passing nil for `typename` however:
+    -- the resulting check would be "is <object>'s type anything other than nil" rather than
+    -- the more likely intended "is <object>'s type nil (in other words, is it nil?)".
+    assert(type(argIdx) == "number")
+    assert(type(typename) == "string")
+
+    if (type(object) ~= typename) then
+        local msg = "argument #"..argIdx.." must be a "..typename
         error(msg, level+1)
     end
 end
