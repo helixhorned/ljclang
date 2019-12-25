e60d90ab1857c2251c43967858ebddb6655f696e
Philipp Kutin <philipp@swarm64.com>
2019-12-25 14:45:34 +0100

    grabscreen.lua: add continuous mode and millisecond timing output to stderr.

--- grabscreen.lua
+++ grabscreen.lua
@@ -6,9 +6,11 @@ local io = require("io")
 local os = require("os")
 
 local FrameBuffer = require("framebuffer").FrameBuffer
+local posix = require("posix")
 
 local arg = arg
 local print = print
+local tonumber = tonumber
 
 ----------
 
@@ -24,11 +26,21 @@ local function Array(elementType, count)
     return ffi.gc(ptrType(voidPtr), ffi.C.free)
 end
 
+local function currentTimeMs()
+    local ts = posix.clock_gettime()
+    return 1000 * tonumber(ts.sec) + tonumber(ts.nsec) / 1000000
+end
+
 ----------
 
-if (arg[1] ~= "-") then
-    print("Usage: "..arg[0].." -")
-    print("Captures the screen to stdout in an unspecified 8-bit format.")
+local continuous = (arg[1] == 'c')
+local toStdout = (arg[1] == '-')
+
+if (not (continuous or toStdout)) then
+    print("Usage: "..arg[0].." [-|c]")
+    print("Captures the screen to in an unspecified 8-bit format.")
+    print(" -: to stdout")
+    print(" c: continuously, without output")
     os.exit(1)
 end
 
@@ -41,12 +53,21 @@ local size = map:getSize()
 local tempBuf = Array(map:getPixelType(), size)
 local narrowBuf = Array(ffi.typeof("uint8_t"), size)
 
--- NOTE: this significantly speeds things up (both on the Pi and the desktop).
-ffi.copy(tempBuf, fbPtr, size * map:getPixelSize())
+local function copyAndNarrow()
+    -- NOTE: this significantly speeds things up (both on the Pi and the desktop).
+    ffi.copy(tempBuf, fbPtr, size * map:getPixelSize())
 
-for i = 0, size - 1 do
-    local r, _g, _b, _a = unpackPx(tempBuf[i])
-    narrowBuf[i] = r
+    for i = 0, size - 1 do
+        local r, _g, _b, _a = unpackPx(tempBuf[i])
+        narrowBuf[i] = r
+    end
 end
 
+repeat
+    local startMs = currentTimeMs()
+    copyAndNarrow()
+    io.stderr:write(("%.0f ms\n"):format(currentTimeMs() - startMs))
+until (not continuous)
+
+-- NOTE: never reached in continuous mode.
 io.write(ffi.string(narrowBuf, size))
