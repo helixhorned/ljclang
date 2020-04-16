a512faacea3f1f1bfba3954930dd7ac3d113ff84
Philipp Kutin <philipp.kutin@gmail.com>
2020-04-16 09:26:19 +0200

    text_editor_bindings: add GPL3 LICENSE.txt

--- text_editor_bindings/ljclang-wcc-flycheck.el
+++ text_editor_bindings/ljclang-wcc-flycheck.el
@@ -14,8 +14,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; TODO: add license file.
-
 (require 'flycheck)
 
 ;; Usage (for now):
