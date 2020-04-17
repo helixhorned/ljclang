61909111514540275725d3ce06f2e4bb8914aa41
Philipp Kutin <philipp.kutin@gmail.com>
2020-04-17 18:53:15 +0200

    Generate surrogate struct definitions for fd_set and sigset_t using 'extractdecls.lua -m'.
    This makes ljposix.cpp retain only a few light dependencies on C++.

--- dev/posix.h
+++ dev/posix.h
@@ -1,5 +1,6 @@
 /* This file is used for extracting entities out of system headers. */
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <poll.h>
-#include <time.h>
+#include <signal.h>
--- ljposix.cpp
+++ ljposix.cpp
@@ -2,60 +2,12 @@
 // Copyright (C) 2013-2020 Philipp Kutin
 // See LICENSE for license information.
 
-#include <string>
-#include <type_traits>
-
 #include <sys/select.h>
 #include <dirent.h>
 #include <poll.h>
 #include <signal.h>
 
-namespace
-{
-    template <typename T> struct TypeString {};
-
-    template <> struct TypeString<sigset_t> {
-        static const std::string structDef;
-        static const char *value;
-    };
-
-    template <> struct TypeString<fd_set> {
-        static const std::string structDef;
-        static const char *value;
-    };
-
-    const std::string TypeString<sigset_t>::structDef =
-        "struct { uint8_t bytes_[" + std::to_string(sizeof(sigset_t)) + "]; } " +
-        "__attribute__((aligned(" + std::to_string(alignof(sigset_t)) + ")))";
-    const char *TypeString<sigset_t>::value = structDef.c_str();
-
-    // NOTE: LuaJIT does not seem to support endowing a C struct with
-    //  __attribute__((aligned(...))) with metamethods. ("Invalid C type".)
-    using fd_mask = long int;
-    static_assert(alignof(fd_set) == alignof(fd_mask));
-    static_assert(sizeof(fd_set) % sizeof(fd_mask) == 0);
-
-    const std::string TypeString<fd_set>::structDef =
-        "struct { long int bytes_[" + std::to_string(sizeof(fd_set) / sizeof(fd_mask)) + "]; }";
-
-    const char *TypeString<fd_set>::value = structDef.c_str();
-}
-
-#define TypeDef(typeName) \
-    std::string{"typedef "} + TypeString<typeName>::value + " " + #typeName + ";"
-
 extern "C" {
-const char *ljclang_getTypeDefs()
-{
-    static const std::string s =
-        // signal.h
-        TypeDef(sigset_t) +
-        // sys/select.h
-        TypeDef(fd_set);
-
-    return s.c_str();
-}
-
 // ---------- fd_set ----------
 
 static_assert(FD_SETSIZE == 8 * sizeof(fd_set));
